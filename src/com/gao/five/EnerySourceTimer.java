b047d7e394c00506390d6a98b86b1dad7d162121
gaopengxiang417 <gaopengxiang417@163.com>
2013-08-22 21:38:48 +0800

    通过定时来调用充电，可以公用线程，不必在充电函数里面用ｗｈｉｌｅ

---
+++ src/com/gao/five/EnerySourceTimer.java
@@ -0,0 +1,62 @@
+package com.gao.five;
+
+import java.util.concurrent.*;
+
+/**
+ * User: wangchen.gpx
+ * Date: 13-8-22
+ * Time: 下午9:24
+ */
+public class EnerySourceTimer {
+    private final Integer MAXLEVEL = 100;
+    private Integer level = MAXLEVEL;
+    private static final ScheduledExecutorService scheduledExecutorService =
+            Executors.newScheduledThreadPool(10 , new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread thread = new Thread(r);
+                    thread.setDaemon(true);
+                    return thread;
+                }
+            });
+    private ScheduledFuture<?> scheduledFuture;
+
+    private EnerySourceTimer(){}
+
+    private void init() {
+        scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                repleash();
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public boolean useEnery(int amount){
+        if (amount > 0 && amount <= level){
+            level -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public static EnerySourceTimer createSource(){
+        EnerySourceTimer enerySourceTimer = new EnerySourceTimer();
+        enerySourceTimer.init();
+        return enerySourceTimer;
+    }
+
+    public void stopEnery(){
+        scheduledFuture.cancel(false);
+    }
+
+    public int getAvaiableLevel(){
+        return level;
+    }
+
+    private void repleash() {
+        if (level < MAXLEVEL)
+            level++;
+    }
+
+}
